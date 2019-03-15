8a4def8a571244f3a90000297d930e5ca275450d
raneemahmed <48535847+raneemahmed@users.noreply.github.com>
2019-03-15 13:40:56 -0700

    Update actions.java

--- project/src/project/actions.java
+++ project/src/project/actions.java
@@ -21,7 +21,7 @@ public class actions extends JFrame {
 
     JButton with = new JButton("Withdraw");
     JButton dep = new JButton("Deposit");
-    JButton bi = new JButton("BalanceInquiry");
+    JButton bi = new JButton("Balance Inquiry");
     JButton prev = new JButton("Previous");
     JButton nex = new JButton("Next");
     JButton back = new JButton("Back");
